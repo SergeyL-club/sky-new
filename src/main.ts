16555a1b4d8f58cfa322b1f0e894195c1e3bf489
SergeyL-club <damnazzel@gmail.com>
2024-07-14 14:14:19 +0500

    add eslint and prettier in typescript

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-const main = async () => {
-    console.log("test run nodemon");
-} 
+const main = () =>
+  new Promise<number>((resolve, reject) => {
+    if (process.argv.length > 10) reject(new Error('Argv length > 10'));
 
-main();
+    console.log('Run Program');
+
+    resolve(1);
+  });
+
+main()
+  .then((code) => process.exit(code))
+  .catch((error: unknown) => {
+    console.error(error);
+    process.exit(0);
+  });
