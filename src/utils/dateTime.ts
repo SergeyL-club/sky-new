2d828e0869334b2d955fc9efa366a41b2b744d6b
SergeyL-club <damnazzel@gmail.com>
2024-07-15 14:17:18 +0500

    add utils date time

---
+++ src/utils/dateTime.ts
@@ -0,0 +1,46 @@
+interface DateForm {
+  day: string;
+  month: string;
+  year: string;
+}
+
+interface MoreDateForm extends DateForm {
+  hours: string;
+  minutes: string;
+  seconds: string;
+  milliseconds: string;
+}
+
+const getDate = <MoreType extends 'base' | 'more' | 'format' | 'formatDate'>({
+  isMore,
+  actualDate,
+}: {
+  actualDate?: Date | number;
+  isMore: MoreType;
+}): MoreType extends 'base' ? DateForm : MoreType extends 'format' ? string : MoreDateForm => {
+  const date = actualDate ? new Date(actualDate) : new Date();
+
+  const dateForm = {} as DateForm;
+
+  dateForm.year = date.getFullYear().toString();
+  dateForm.month = date.getMonth().toString().length == 1 ? '0' + date.getMonth() : date.getMonth().toString();
+  dateForm.day = date.getDay().toString().length == 1 ? '0' + date.getDay() : date.getDate().toString();
+
+  if (isMore === 'base') return dateForm as any;
+
+  const moreDateForm = { ...dateForm } as MoreDateForm;
+
+  moreDateForm.hours = date.getHours().toString().length == 1 ? '0' + date.getHours() : date.getHours().toString();
+  moreDateForm.minutes = date.getMinutes().toString().length == 1 ? '0' + date.getMinutes() : date.getMinutes().toString();
+  moreDateForm.seconds = date.getSeconds().toString().length == 1 ? '0' + date.getMinutes() : date.getSeconds().toString();
+  moreDateForm.milliseconds = date.getMilliseconds().toString();
+
+  if (isMore === 'format')
+    return `${moreDateForm.year}-${moreDateForm.month}-${moreDateForm.day} ${moreDateForm.hours}:${moreDateForm.minutes}:${moreDateForm.seconds}` as any;
+
+  if (isMore === 'formatDate') return `${moreDateForm.year}-${moreDateForm.month}-${moreDateForm.day}` as any;
+
+  return moreDateForm as any;
+};
+
+export { getDate };
