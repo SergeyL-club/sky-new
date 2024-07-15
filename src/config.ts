9cf789eb775e995d56aa4006f2fa89776152a0eb
SergeyL-club <damnazzel@gmail.com>
2024-07-15 11:15:57 +0500

    set start config

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// порт запуска на сервере (запросы по этому порту отправлять)
+const PORT = 13004;
+
+// майл и пороль для входа в sky
+const MAIL = 'support@bizoninvest.com';
+const PASSWORD = 'fMwbh7XeXdTYNci';
+
+export { PORT, MAIL, PASSWORD };
