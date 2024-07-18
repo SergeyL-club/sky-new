f4159602b5983892a80a4ffb41a5b2ffe690b113
SergeyL-club <damnazzel@gmail.com>
2024-07-18 12:12:38 +0500

    add timer

---
+++ src/utils/timer.ts
@@ -0,0 +1,44 @@
+import * as CONFIG from '../config.js';
+import { delay } from './dateTime.js';
+
+class Timer {
+  private static instance: Timer;
+
+  constructor() {
+    if (Timer.instance) return Timer.instance;
+    Timer.instance = this;
+  }
+
+  CYCLE_TOGGLE = {
+    IS_WHILE_DEALS: CONFIG['IS_WHILE_DEALS'],
+    IS_WHILE_KEYS: CONFIG['IS_WHILE_KEYS'],
+  };
+
+  CYCLE_DELAY = {
+    IS_WHILE_DEALS: CONFIG['DELAY_UPDATE_DEALS'],
+    IS_WHILE_KEYS: CONFIG['DELAY_UPDATE_KEYS'],
+  };
+
+  cycle(hand: keyof typeof this.CYCLE_TOGGLE, func: () => void | Promise<void>) {
+    if (this.CYCLE_TOGGLE[hand]) {
+      func();
+    }
+    delay(this.CYCLE_DELAY[hand]).finally(() => this.cycle(hand, func));
+  }
+
+  timer(hand: keyof typeof this.CYCLE_TOGGLE, func: () => void | Promise<void>) {
+    if (this.CYCLE_TOGGLE[hand]) {
+      let time = Date.now();
+      if (func)
+        Promise.resolve(func()).finally(() => {
+          let timeDelay = this.CYCLE_DELAY[hand];
+          let nextTime = Date.now();
+          if (nextTime - time >= this.CYCLE_DELAY[hand]) timeDelay = 0;
+          else timeDelay = nextTime - time;
+          delay(timeDelay).finally(() => this.timer(hand, func));
+        });
+    } else delay(this.CYCLE_DELAY[hand]).finally(() => this.timer(hand, func));
+  }
+}
+
+export default new Timer();
