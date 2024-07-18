70c6fe1b4a8d24dc7aed6932e67ca89e194842c9
SergeyL-club <damnazzel@gmail.com>
2024-07-18 12:13:10 +0500

    create worker browser and testing in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,48 @@
+import path from 'path';
+import { wrap } from 'comlink';
+import type { Remote } from 'comlink';
 import logger from './utils/logger.js';
-import browser from './utils/browser.js';
+import { Worker } from 'node:worker_threads';
+import type { TransferListItem } from 'node:worker_threads';
+import type { WorkerBrowser } from './workers/browser.js';
+import { delay } from './utils/dateTime.js';
 
 const main = () =>
   new Promise<number>((_, reject) => {
+    // создание потока на browser
+    logger.info('Создание отдельного потока для browser');
+    const workerBrowser = new Worker(path.resolve(import.meta.dirname, './workers/browser.js'));
+    const workerBrowserAdapter = new MessageChannel();
+
+    // задать отключение нужных параметров при неизвестном выходе процесса (его закрытие, пример: когда дебаг поток на браузер остается если его принудильно закрыть)
+    process.on('exit', () => {
+      workerBrowser.postMessage({ exit: 1 });
+    });
+
+    // код после запуска browser
+    const next = async (browser: Remote<WorkerBrowser>, code: boolean) => {
+      if (!code) new Error('Не удалось инициализировать browser');
+      browser.loop();
+      await delay(20000);
+      const paramsBtcOne = {
+        symbol: 'btc',
+        currency: 'rub',
+        offset: 0,
+        limit: 10,
+      };
+      const codeGet = `new Promise((resolve) => getDeals('[authKey]', ${JSON.stringify(paramsBtcOne)}).then(resolve).catch(() => resolve([])))`;
+      let result = await browser.evalute({ code: codeGet });
+      logger.log({ obj: { btcOne: result } }, 'Результат запроса на список сделок');
+    };
+
     try {
-      browser.initBrowser();
+      // инициализация browser (его привязка)
+      logger.info('Привязка потока browser к core потоку');
+      const browser = wrap<WorkerBrowser>(workerBrowserAdapter.port1);
+      // workerBrowser.once('message', next.bind(this, browser));
+      workerBrowser.postMessage({ port: workerBrowserAdapter.port2 }, [workerBrowserAdapter.port2 as unknown as TransferListItem]);
     } catch (error: unknown) {
+      workerBrowser.postMessage({ exit: 1 });
       reject(error);
     }
   });
---
+++ src/workers/browser.ts
@@ -0,0 +1,380 @@
+import type { InputSet, Keys, Params, ProxyData } from '../../types/workers/browser.type.js';
+import stealsPlugin from 'puppeteer-extra-plugin-stealth';
+import type { VanillaPuppeteer } from 'puppeteer-extra';
+import { delay, random } from '../utils/dateTime.js';
+import { loggerBrowser } from '../utils/logger.js';
+import { parentPort } from 'node:worker_threads';
+import { PuppeteerExtra } from 'puppeteer-extra';
+import type { Browser, Page } from 'puppeteer';
+import * as CONFIG from '../config.js';
+import { readdirSync } from 'node:fs';
+import timer from '../utils/timer.js';
+import { resolve } from 'node:path';
+import puppeteer from 'puppeteer';
+import { expose } from 'comlink';
+import md5 from 'md5';
+
+export class WorkerBrowser {
+  private static instance: WorkerBrowser;
+
+  // init data
+  private proxyParams: ProxyData | string;
+
+  // local params
+  public isReAuth: boolean;
+
+  // browser data
+  private keys: Keys;
+  private authKey: string;
+  private deals: Page | null;
+  private browser: Browser | null;
+
+  constructor() {
+    this.proxyParams = '';
+    this.isReAuth = false;
+    this.browser = null;
+    this.deals = null;
+    this.authKey = '';
+    this.keys = {};
+    if (WorkerBrowser.instance) return WorkerBrowser.instance;
+    WorkerBrowser.instance = this;
+  }
+
+  loop = () => {
+    loggerBrowser.log('loop browser worker');
+  };
+
+  generateAuthKey = (keys?: Keys) => {
+    const localKeys = keys ?? this.keys;
+    const t = 'e' === localKeys['aKM'];
+    let r = Object.keys(localKeys).sort();
+    if (t) r = r.reverse();
+    const o =
+      r
+        .map(function (t) {
+          return localKeys[t];
+        })
+        .join('') + 'l';
+    return md5(o);
+  };
+
+  initProxy = (proxy: string = '') => {
+    if (proxy != '') {
+      const info = new URL(proxy);
+      this.proxyParams = {
+        url: info.origin,
+        user: info.username,
+        pass: info.password,
+      };
+    }
+  };
+
+  initParams = () => {
+    const params = {} as Params;
+    if (process.argv.includes('--headless')) params['headless'] = true;
+    else params['headless'] = !true;
+    params['args'] = ['--no-sandbox', '--no-default-browser-check', ...(this.proxyParams && [`--proxy-server=${(<ProxyData>this.proxyParams).url}`])];
+    params['defaultViewport'] = { width: 1100, height: 600 };
+    params['ignoreDefaultArgs'] = ['--enable-automation'];
+    params['userDataDir'] = `./user_data`;
+    return params;
+  };
+
+  incPage = async (page: Page, url: string) => {
+    loggerBrowser.info('Установка конфигурации страницы', `(${url})`);
+    await page.setBypassCSP(true);
+    if (typeof this.proxyParams != 'string')
+      await page.authenticate({
+        username: this.proxyParams.user,
+        password: this.proxyParams.pass,
+      });
+
+    await page.evaluateOnNewDocument(`
+			navigator.webkitGetUserMedia =
+			navigator.mozGetUserMedia =
+			navigator.getUserMedia =
+			webkitRTCPeerConnection =
+			RTCPeerConnection =
+			MediaStreamTrack = undefined;
+			if (navigator.mediaDevices)
+				navigator.mediaDevices.getUserMedia = undefined;`);
+    await page.setUserAgent('Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36');
+  };
+
+  injectStatic = async (page: Page, socket: boolean = true) => {
+    // await page.waitForNavigation({ timeout: CONFIG['WAIT_TIMEOUT'], waitUntil: CONFIG['WAIT_UNTIL'] });
+    const files: string[] = readdirSync(resolve(import.meta.dirname, `../../statics`));
+
+    for (let index = 0; index < files.length; index++) {
+      const fileName = files[index];
+      if (!socket && fileName == 'socket.io.refresh.js') continue;
+      await page.addScriptTag({ path: resolve(import.meta.dirname, `../../statics/${fileName}`) });
+    }
+  };
+
+  initPage = async (url: string) => {
+    let page: Page | null = null;
+    if (!this.browser) {
+      loggerBrowser.warn('При создание page необходим browser, но он является null');
+      return null;
+    }
+    page = await this.browser.newPage();
+    await this.incPage(page, url);
+    await this.reload(url, page);
+    return page;
+  };
+
+  initBrowser = async () => {
+    try {
+      loggerBrowser.info('Установка конфигурации браузера');
+      const params = this.initParams();
+
+      // close old browser
+      if (this.browser) await this.browser.close();
+      const puppeteerExtra = new PuppeteerExtra(puppeteer as unknown as VanillaPuppeteer);
+      puppeteerExtra.use(stealsPlugin());
+
+      this.browser = await puppeteerExtra.launch(params);
+
+      // set page deals
+      const url = 'https://skycrypto.me/deals';
+      let page = await this.initPage(url);
+      if (!page) {
+        loggerBrowser.error(new Error('Ошибка создания page'), 'Не удалось создать page deals');
+        return false;
+      }
+
+      // check auth
+      page = await this.checkAuth(page, url);
+      if (!page) {
+        loggerBrowser.error(new Error('Ошибка авторизации page'), 'Не удалось провести авторизацию page deals');
+        return false;
+      }
+
+      // close 0 page
+      loggerBrowser.log('Удаляем ненужную страницу');
+      const pageNull = (await this.browser.pages())[0];
+      await pageNull.close();
+
+      // save page
+      loggerBrowser.log('Сохранияем адрес ссылки на сделки');
+      this.deals = page;
+
+      // set socket in keys
+      this.updateKeys(this.deals);
+
+      // end
+      loggerBrowser.info('Установка базовой конфигурации завершена');
+      return true;
+    } catch (error: unknown) {
+      loggerBrowser.error(error, 'Ошибка без обработки (init browser)');
+      return false;
+    }
+  };
+
+  waitReAuth = (): Promise<boolean> =>
+    new Promise((res) => {
+      let cnt = 0;
+      let isDelay = false;
+
+      while (cnt < 250) {
+        isDelay = true;
+        delay(100).then(() => (isDelay = false));
+        if (!this.isReAuth) {
+          res(true);
+          return;
+        }
+        if (!isDelay) cnt++;
+      }
+      res(false);
+    });
+
+  reload = async (url: string, page: Page, count = 1): Promise<void> => {
+    try {
+      await this.goto(page, 'about:blank');
+      await this.goto(page, url);
+    } catch (error: unknown) {
+      if (count < 4) {
+        loggerBrowser.warn({ err: error }, `Не удачная перезагрузка страницы (reload)`);
+        return await this.reload(url, page, count + 1);
+      }
+      loggerBrowser.error(error, 'Окончательная ошибка перезагрузки страницы (reload)');
+    }
+  };
+
+  goto = async (page: Page, url: string) => {
+    await page.goto(url, { timeout: CONFIG['WAIT_TIMEOUT'], waitUntil: CONFIG['WAIT_UNTIL'] });
+  };
+
+  inputSet = async ({ input, page, text }: InputSet) => {
+    try {
+      input.focus();
+      if (page) {
+        await input.click({ clickCount: 3, delay: random(50, 100) });
+        await delay(random(150, 200));
+        await page.keyboard.press('Backspace');
+      }
+
+      if (text.length <= 25) await input.type(text, { delay: random(100, 150) });
+      else await input.type(text);
+
+      loggerBrowser.log(`Запись в input завершёна, text: ${text}`);
+      return true;
+    } catch (error: unknown) {
+      loggerBrowser.warn({ err: error }, 'Ошибка при печати input');
+      return false;
+    }
+  };
+
+  checkAuth = async (page: Page, url = 'https://skycrypto.me/wallet') => {
+    loggerBrowser.log(`Проверка авторизации (${url})`);
+    let localPage: Page | null = page;
+
+    if (this.isReAuth) {
+      loggerBrowser.log('Ждём авторизации');
+      await localPage.close();
+
+      if (!(await this.waitReAuth())) {
+        loggerBrowser.warn('Не должался авторизации (check auth)');
+        return null;
+      }
+      localPage = await this.initPage(url);
+      if (localPage) await this.goto(localPage, url);
+      else {
+        loggerBrowser.warn('Пустой page (check auth)');
+        return null;
+      }
+    }
+
+    const error = (await localPage.$(CONFIG['SELECTOR_ERROR'][0])) || (await localPage.$(CONFIG['SELECTOR_ERROR'][1])) || null;
+    const checkMain = localPage.url() == CONFIG['URL_MAIN_AUTH'] || localPage.url() == `${CONFIG['URL_MAIN_AUTH']}/`;
+
+    if (error || checkMain || localPage.url() != url) {
+      this.isReAuth = true;
+      loggerBrowser.log('Не авторизирован, попытка авторизироваться');
+      await delay(random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']));
+      if (!(await this.auth(localPage))) return null;
+      loggerBrowser.log('Переходит на нужную ссылку');
+      await this.goto(localPage, url);
+      this.isReAuth = false;
+    }
+
+    loggerBrowser.log('Завершили проверку авторизации');
+
+    await delay(random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']));
+    return page;
+  };
+
+  auth = async (page: Page) => {
+    loggerBrowser.log('Переход на главную страницу');
+    await this.goto(page, CONFIG['URL_MAIN_AUTH']);
+
+    const btnAuth = (await page.$(CONFIG['SELECTOR_AUTH_FORM'][0])) || (await page.$(CONFIG['SELECTOR_AUTH_FORM'][1])) || null;
+    if (!btnAuth) {
+      loggerBrowser.warn('Неудалось найти кнопку входа (главная страница)');
+      return false;
+    }
+    await btnAuth.click({ delay: random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']) });
+
+    let inputEmail = (await page.$(CONFIG['SELECTOR_INPUT_EMAIL'])) || null;
+    let inputPassword = (await page.$(CONFIG['SELECTOR_INPUT_PASSWORD'])) || null;
+    if (!inputEmail || !inputPassword) {
+      const uriAuth = (await page.$(CONFIG['SELECTOR_URL_AUTH'][0])) || (await page.$(CONFIG['SELECTOR_URL_AUTH'][1])) || null;
+      if (!uriAuth) {
+        loggerBrowser.warn('Не было найдена "a" для открытия input password');
+        return false;
+      }
+      await uriAuth.click({ delay: random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']) });
+    }
+
+    await delay(CONFIG['DELAY_AUTH']);
+    inputEmail = (await page.$(CONFIG['SELECTOR_INPUT_EMAIL'])) || null;
+    inputPassword = (await page.$(CONFIG['SELECTOR_INPUT_PASSWORD'])) || null;
+    if (!inputEmail || !inputPassword) {
+      loggerBrowser.warn('Даже после нажатия на "a" для открытия input password он так и не появлися');
+      return false;
+    }
+    await delay(random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']));
+    await this.inputSet({ input: inputEmail, text: CONFIG['EMAIL'], page });
+    await delay(random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']));
+    await this.inputSet({ input: inputPassword, text: CONFIG['PASSWORD'], page });
+
+    const btnAuthForm = (await page.$(CONFIG['SELECTOR_BTN_AUTH'][0])) || (await page.$(CONFIG['SELECTOR_BTN_AUTH'][1])) || null;
+    if (!btnAuthForm) {
+      loggerBrowser.warn('Неудалось найти кнопку входа (форма)');
+      return false;
+    }
+    await btnAuthForm.click({ delay: random(CONFIG['DELAY_EVENT_MIN'], CONFIG['DELAY_EVENT_MAX']) });
+    await delay(CONFIG['DELAY_AUTH']);
+
+    return true;
+  };
+
+  closeBrowser = async () => {
+    await this.browser?.close();
+  };
+
+  updateKeys = async (page?: Page) => {
+    loggerBrowser.log('Запуск сокета для обновления keys и auth key');
+    const localPage = page ?? this.deals;
+    if (!localPage) {
+      loggerBrowser.warn('Не удалось запустить socket для обновления keys (auth key), т.к. нету page');
+      return;
+    }
+
+    await this.injectStatic(localPage);
+    timer.timer('IS_WHILE_KEYS', async () => {
+      try {
+        let nextKeys = ((await localPage.evaluate('keys')) as Keys) || ({} as Keys);
+        if (JSON.stringify(nextKeys) !== JSON.stringify(this.keys)) {
+          this.keys = nextKeys;
+          let localAuthKey = this.generateAuthKey(nextKeys);
+          loggerBrowser.log(`Изменение auth key: ${this.authKey == '' ? 'null' : this.authKey}, ${localAuthKey}`);
+          this.authKey = localAuthKey;
+        }
+      } catch (error: unknown) {
+        loggerBrowser.warn({ err: error }, 'Не получилось получить ключи для auth key');
+      }
+    });
+  };
+
+  evalute = async <Type>({ page, code }: { page?: Page; code: string }) => {
+    loggerBrowser.log(`Запрос на browser, код: ${code}`);
+    // проверяем page
+    if (page) this.injectStatic(page, false);
+    const localPage = page ?? this.deals;
+    if (!localPage) {
+      loggerBrowser.warn('Не удалось сделать запрос, т.к. отсутсвует page');
+      return null;
+    }
+
+    // проверка регулярок
+    let localCode = `${code}`;
+    loggerBrowser.log('Поиск регулярок и их замена');
+    if (localCode.includes('[authKey]')) localCode = localCode.split('[authKey]').join(`${this.authKey}`);
+
+    // делаем запрос и отдаем ответ
+    loggerBrowser.log('Производим запрос');
+    const result = await localPage.evaluate(localCode);
+    loggerBrowser.log('Запрос прошёл успешно, отправляем ответ');
+    return result as Type;
+  };
+}
+
+const workerBrowser = new WorkerBrowser();
+workerBrowser.initBrowser().then((code) => {
+  workerBrowser.updateKeys().then(() => parentPort?.postMessage(code));
+});
+
+parentPort?.once('message', (message) => {
+  const { port } = message;
+  expose(workerBrowser, port);
+});
+
+parentPort?.on('message', async (msg) => {
+  if ('exit' in msg) {
+    timer.CYCLE_TOGGLE.IS_WHILE_KEYS = false;
+    await workerBrowser.closeBrowser();
+    process.exit(1);
+  }
+});
