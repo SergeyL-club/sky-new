12cf195e04ea8cebf45d768d158056786addd505
SergeyL-club <damnazzel@gmail.com>
2024-08-16 13:45:11 +0500

    add request statics

---
+++ statics/cancel.deal.js
@@ -0,0 +1,36 @@
+const cancelDeal = async (key, dealId) =>
+  new Promise((resolve, reject) => {
+    const data = { deal_id: dealId };
+    const headers = {
+      Authorization: `Bearer ${getCookie('sky_token')}`,
+      AuthKey: key,
+      'Access-Control-Allow-Origin': '*',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36',
+      Accept: 'applicaton/json, text/plain, */*',
+      'Accept-Encoding': 'gzip, deflate, br, zstd',
+      'Accept-Language': 'en-US',
+      'Content-Length': `${JSON.stringify(data).length}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    };
+
+    const url = 'https://api.skycrypto.me/rest/v1/deals/cancel';
+
+    fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .then((e) => {
+        resolve(e);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
